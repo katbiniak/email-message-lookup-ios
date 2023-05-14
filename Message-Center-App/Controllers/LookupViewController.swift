fec04bf459f56e542515c028815c164f9fae45c2
Katelyn Biniak <katelyn.biniak@razorfish.com>
2023-05-13 22:47:27 -0400

    Initial project setup, adding assets and accent color needed for views, organizing initial VCs and storyboards

---
+++ Message-Center-App/Controllers/LookupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LookupViewController.swift
+//  Message Center App
+//
+//  Created by Katelyn Biniak on 5/11/23.
+//
+
+import UIKit
+
+class LookupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
